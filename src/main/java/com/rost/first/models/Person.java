43712002ecfe62c9998137e3d8f010e6162f9dc1
Rost <coderost@yandex.ru>
2023-01-06 16:17:16 +0300

    Create entity and implements the "Null-Object pattern".

--- src/main/java/com/rost/first/models/Person.java
+++ src/main/java/com/rost/first/models/Person.java
@@ -1,5 +1,43 @@
 package com.rost.first.models;
 
-public class Person {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "email")
+    private String email;
+
+    public static final Person NULL_OBJECT = new Person(0, "John Doe", 0, "nobody@gmail.com");
+
+    @Override
+    public String toString() {
+        return String.format("%s, %d; %s", name, age, email);
+    }
 }
