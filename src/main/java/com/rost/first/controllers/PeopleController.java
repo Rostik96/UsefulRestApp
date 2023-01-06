48789297e726a35a43ced3fff341ee2eb73d9309
Rost <coderost@yandex.ru>
2023-01-06 18:16:35 +0300

    Add creation method in PeopleController

--- src/main/java/com/rost/first/controllers/PeopleController.java
+++ src/main/java/com/rost/first/controllers/PeopleController.java
@@ -2,11 +2,17 @@ package com.rost.first.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +33,16 @@ public class PeopleController {
         return peopleService.readAll(); //Jackson под капотом конвертирует объекты в JSON.
     }
 
+    @PostMapping
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            //TODO: дописать логику
+        }
+        peopleService.save(person);
+        // Отправляем HTTP ответ с пустым телом и со статусом 200
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
     @GetMapping("/{id}")
     public Person getPerson(@PathVariable("id") int id) {
         return peopleService.findOne(id);
@@ -38,6 +54,7 @@ public class PeopleController {
                 "Person with this ID wasn't found!",
                 System.currentTimeMillis()
         );
+        e.printStackTrace();
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND); //NOT_FOUND - 404 статус
     }
 }
