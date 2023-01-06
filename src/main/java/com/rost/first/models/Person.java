763115457a44b16d95ad6e7c200ada07b7eeb38b
Rost <coderost@yandex.ru>
2023-01-06 18:01:09 +0300

    Add validation constraints onto Person's fields.

--- src/main/java/com/rost/first/models/Person.java
+++ src/main/java/com/rost/first/models/Person.java
@@ -6,6 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -20,21 +24,27 @@ import lombok.Setter;
 @Entity
 @Table(name = "person")
 public class Person {
+    public static final Person NULL_OBJECT = new Person(0, "John Doe", 0, "nobody@gmail.com");
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters inclusive")
     private String name;
 
     @Column(name = "age")
+    @Min(value = 0, message = "Age should be greater or equals 0")
     private int age;
 
     @Column(name = "email")
+    @Email
+    @NotEmpty(message = "E-mail should not be empty")
     private String email;
 
-    public static final Person NULL_OBJECT = new Person(0, "John Doe", 0, "nobody@gmail.com");
 
     @Override
     public String toString() {
