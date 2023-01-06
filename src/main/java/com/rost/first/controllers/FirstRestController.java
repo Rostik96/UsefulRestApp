b4dadc91839800634fa5a421922af67f9c0a10cc
Rost <coderost@yandex.ru>
2023-01-06 14:38:02 +0300

    Create simple trivial FirstRestController

--- src/main/java/com/rost/first/controllers/FirstRestController.java
+++ src/main/java/com/rost/first/controllers/FirstRestController.java
@@ -4,21 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.rost.first.models.Person;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class FirstRestController {
-    @ResponseBody
     @GetMapping("/sayHello")
     public String sayHello() {
         return "Hello world";
-    }
-
-    @ResponseBody
-    @GetMapping("/new-person")
-    public Person getNewPerson() {
-        return new Person();
     }
 }
