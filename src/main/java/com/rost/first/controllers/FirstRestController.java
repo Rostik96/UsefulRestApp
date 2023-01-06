31e8337377c7a38a006f3fb76fc06e1c0aa20675
Rost <coderost@yandex.ru>
2023-01-06 16:18:05 +0300

    Create simple FirstRestController

--- src/main/java/com/rost/first/controllers/FirstRestController.java
+++ src/main/java/com/rost/first/controllers/FirstRestController.java
@@ -1,14 +1,10 @@
 package com.rost.first.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rost.first.models.Person;
-
-@RestController
+@RestController(value = "df")
 @RequestMapping("/api")
 public class FirstRestController {
     @GetMapping("/sayHello")
