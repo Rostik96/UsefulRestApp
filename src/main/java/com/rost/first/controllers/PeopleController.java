54330c72c5ad67aefb4ee2fcd2e456faba193ab8
Rost <coderost@yandex.ru>
2023-01-06 17:02:03 +0300

    Create class for HttpEntity's body in PeopleController#getPerson

--- src/main/java/com/rost/first/controllers/PeopleController.java
+++ src/main/java/com/rost/first/controllers/PeopleController.java
@@ -2,6 +2,9 @@ package com.rost.first.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rost.first.models.Person;
 import com.rost.first.services.PeopleService;
+import com.rost.first.util.PersonErrorResponse;
+import com.rost.first.util.PersonNotFoundException;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -25,5 +30,14 @@ public class PeopleController {
     @GetMapping("/{id}")
     public Person getPerson(@PathVariable("id") int id) {
         return peopleService.findOne(id);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<PersonErrorResponse> handleException(PersonNotFoundException e) {
+        PersonErrorResponse response = new PersonErrorResponse(
+                "Person with this ID wasn't found!",
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND); //NOT_FOUND - 404 статус
     }
 }
