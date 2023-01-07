4260d2a66152825794a40a0df661d3c8d2dc92ae
Rost <coderost@yandex.ru>
2023-01-07 18:08:23 +0300

    Add ModelMapper bean

--- src/main/java/com/rost/first/Application.java
+++ src/main/java/com/rost/first/Application.java
@@ -1,13 +1,30 @@
 package com.rost.first;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.rost.first.services.PeopleService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @SpringBootApplication
-public class Application {
-
+public class Application implements CommandLineRunner {
+	private final PeopleService peopleService;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) {
+		peopleService.readAll().forEach(System.out::println);
+	}
 }
