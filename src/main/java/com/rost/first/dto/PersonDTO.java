78f744dcf6b80216b3a9ef6c7137cf859375e85c
Rost <coderost@yandex.ru>
2023-01-07 17:48:27 +0300

    Create DTO class for "Person" entity

---
+++ src/main/java/com/rost/first/dto/PersonDTO.java
@@ -0,0 +1,19 @@
+package com.rost.first.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class PersonDTO {
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters inclusive")
+    private String name;
+
+    @Min(value = 0, message = "Age should be greater or equals 0")
+    private int age;
+
+    @Email
+    @NotEmpty(message = "E-mail should not be empty")
+    private String email;
+}
