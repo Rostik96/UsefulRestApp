14480d6741a49f3e09e808c9d2f1ea4aad48bdaf
Rost <coderost@yandex.ru>
2023-01-07 17:59:02 +0300

    Add getters and setters to DTO class.

--- src/main/java/com/rost/first/dto/PersonDTO.java
+++ src/main/java/com/rost/first/dto/PersonDTO.java
@@ -5,6 +5,11 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PersonDTO {
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters inclusive")
